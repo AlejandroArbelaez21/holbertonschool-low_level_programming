4e2d89588bebd8542aac2af908cc1cb27aa02cca
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-09-18 20:19:30 +0000

    C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc $CFILE
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+  char str1[15];
+
+  strcpy(str1, "\"Programming is like building a multilingual puzzle");
+
+  puts(str1);
+   
+  return(0);
+}

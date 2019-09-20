12736bef30f7f88a4bb2e9838ae17a27fa9b8cd2
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2019-09-20 04:39:00 +0000

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int ch;
+int ph;
+for (ch = '0'; ch <= '9'; ch++)
+{
+for (ph = '0'; ph <= '9'; ph++)
+{
+putchar(ch);
+putchar(ph);
+if (ch == '9'&& ph =='9')
+{
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+
+return (0);
+}

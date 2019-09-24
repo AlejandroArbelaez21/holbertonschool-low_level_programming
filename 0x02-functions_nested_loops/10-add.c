8e9e1c25b76f1f557e2b979ea879af8672751107
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-09-24 21:47:25 +0000

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,8 @@
 #include "holberton.h"
 /**
  * add - writes the character c to stdout
+ * @a : integer
+ * @b : integer
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.

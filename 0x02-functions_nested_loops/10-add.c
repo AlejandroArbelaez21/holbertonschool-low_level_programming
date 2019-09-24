91925f6fcd32ef8f758ec7a7b76718ae5e0e4e9c
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-09-24 21:42:54 +0000

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,8 @@
+#include "holberton.h"
+/**
+ * jack_bauer - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void times_table(void)

15a3d10a847e6cccfb2669bc66dc9eb0d2e5967d
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-09-24 23:07:54 +0000

    0x03. C - Debugging

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,27 @@
-#include "holberton.h"
+	#include "holberton.h"
 /**
- * jack_bauer - writes the character c to stdout
+ * times_table - writes the character c to stdout
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 void times_table(void)
+{
+char a, b;
+for (a = '0'; a <= '9'; a++)
+{
+	for (b = '0'; b <= '9'; b++)
+	_putchar(b);
+	if (a == '9' && b == '9')
+	{
+	}
+	else
+	{
+	_putchar(',');
+        _putchar(' ');
+        _putchar(' ');
+	}
+_putchar('\n');
+	}
+return;
+}

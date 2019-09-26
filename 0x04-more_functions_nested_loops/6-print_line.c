8abadae9a640c1b62feaac74ac9456dc297d5073
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-09-26 21:23:16 +0000

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_line - writes the character c to stdout
+ * @n: to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_line(int n)
+{
+int x;
+if (n > 0)
+{
+	for (x = 0; x < n; x++)
+	{
+	_putchar ('_');
+	_putchar ('\n');
+	}
+}
+else
+	{
+_putchar ('\n');
+	}
+}

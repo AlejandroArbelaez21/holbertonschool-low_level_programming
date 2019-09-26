2cb5bcd2d2c812d9c16d6bf99bba364671e02e0e
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-09-26 17:42:00 +0000

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 /**
- * _isdigit - writes the character c to stdout
- * @c: The character to print
+ * mul - writes the character c to stdout
+ * @a: The character to print
+ * @b: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,11 @@
 #include "holberton.h"
 /**
  * print_numbers - writes the character c to stdout
- * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-void print_numbers (void)
+void print_numbers(void)
 {
 int c;
 for (c = 48; c <= 57; c++)

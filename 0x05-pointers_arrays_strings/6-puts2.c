6dd753a7fe736cde532af4f216b76c42b1114652
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-10-01 23:18:31 +0000

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * puts2 - writes the character c to stdout
- * @s: The character to print
+ * @str: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.

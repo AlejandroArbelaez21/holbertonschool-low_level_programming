eb280e8d1b0811c8345b6f7f13c2a8f2568a7622
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2019-10-02 04:52:58 +0000

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * print_array - writes the character c to stdout
- * @a: The character to print
- * @b: The character to print
+ * _strcpy - writes the character c to stdout
+ * @dest: The character to print
+ * @src: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
@@ -12,7 +12,7 @@ char *_strcpy(char *dest, char *src)
 {
 int a = 0;
 int b;
-for (a = 0; src[a] != '\0'; a+= 1)
+for (a = 0; src[a] != '\0'; a += 1)
 	{
 	}
 for (b = 0; b <= a; b++)

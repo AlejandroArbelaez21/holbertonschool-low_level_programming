f5e13329ecd95c1eb06957e7077b0c2ded9f522d
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-10-03 23:31:43 +0000

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- * reverse_array - writes the character c to stdout
- * @a: The character to print
- * @n: The character to print
+ * string_toupper - writes the character c to stdout
+ * @s: The character to print
+ *
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.

b90bfaae163223be846192f19edd4fd4ae45a0f4
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-10-03 17:19:17 +0000

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,6 +17,14 @@ y++;
 return (y);
 }
 
+/**
+ * _strcat - writes the character c to stdout
+ * @src: The character to print
+ * @dest: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
 char *_strcat(char *dest, char *src)
 {

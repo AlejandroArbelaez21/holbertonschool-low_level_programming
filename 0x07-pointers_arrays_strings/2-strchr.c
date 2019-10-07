0a63f9b6042212130197712a645a26df34193dbe
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-10-07 18:29:26 +0000

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
 /**
- * _memcpy - writes the character c to stdout
- * @dest: The character to print
- * @src: The character to print
- * @n: The character to print
+ * _strchr - writes the character c to stdout
+ * @s: The character to print
+ * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
@@ -14,6 +13,10 @@ int i;
 for (i = 0; s[i] != '\0'; i++)
 	{
 	if (s[i] == c)
-	return(c);
+return (s + i);
 	}
+if (s[i] == c)
+return (s + i);
+else
+return ('\0');
 }

6fc56ce612c06ee4afeae8f90a51876909e5e4ac
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-10-07 21:16:05 +0000

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strpbrk - writes the character c to stdout
+ * @s: The character to print
+ * @accept: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i, x;
+for (i = 0; s[i] != '\0'; i++)
+for (x = 0; accept[x] != '\0'; x++)
+	{
+	if (s[i] == accept[x])
+	return (s + i);
+	}
+if (s[i] == accept[i])
+return (s + i);
+else
+return ('\0');
+}

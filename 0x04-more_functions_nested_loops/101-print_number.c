125f4b23cd03b76a8f6114faa0331352ddc1f0ba
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2019-10-08 03:41:26 +0000

    advanced

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * print_number - writes the character n to stdout
+ * @n: print the number
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_number(int n)
+{
+}
+return (n);

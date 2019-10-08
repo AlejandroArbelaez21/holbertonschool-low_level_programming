2be08275fd61d710aab1d14ea8df1c52bcc7ac82
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-10-08 23:15:23 +0000

    correction

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,21 +3,24 @@
 /**
  * print_diagsums - writes the character c to stdout
  * @a: The character to print
+ * @size: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 void print_diagsums(int *a, int size)
 {
-int i, x, sum = 0;
-for (i = 0; a[i] <= size; i++)
+int i, x, sum = 0, sum1 = 0;
+for (i = 0; i < size * size; i++)
 	{
-	for (x = 0; a[x] <= size; x++)
+	sum = sum + a[i];
+	i = i + size;
+	}
+printf("%d, ", sum);
+for (x = (size - 1); x <= (size * size) - size; x++)
 	{
-	if (a[i] == a[x])
-	{
-sum = sum + a[x];
+	sum1 = sum1 + a[x];
+	x = x + (size - 2);
 	}
-	}
-	}
+printf("%d\n", sum1);
 }

8a01a91c3d887e1b580882bce3097d90085fe29b
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-10-15 19:46:10 +0000

    0x0B. C - malloc, free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strdup - writes the character n to stdout
+ * @str: Size character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strdup(char *str)
+{
+unsigned int a, b;
+char *c;
+for (a = 0; str[a] != '\0'; a++)
+	{
+	}
+c = NULL;
+c = malloc(a * sizeof(char));
+if (c == NULL || str == 0)
+	{
+	return (NULL);
+	}
+for (b = 0; b < a; b++)
+	{
+	c[b] = str[b];
+}
+return (c);
+}

89682468abeb90e869a40e137a34a837eb4938de
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-10-17 19:55:51 +0000

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,20 +10,21 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-int *a;
+char *a;
 unsigned int x;
 if (nmemb == 0 || size == 0)
 	{
 	return (NULL);
 	}
-for (x = 0; x < nmemb; x++)
-	{
 a = 0;
-a = malloc(x * sizeof(size));
+a = malloc(nmemb * sizeof(size));
 	if (a == NULL)
 	{
 	return (NULL);
 	}
-	}
+for (x = 0; x < nmemb * size; x++)
+        {
+        a[x] = 0;
+        }
 return (a);
 }

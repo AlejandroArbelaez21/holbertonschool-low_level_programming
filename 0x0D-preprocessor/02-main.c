4c78a560154a3fa718e220ffe5c178370533fe2f
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-10-21 17:57:32 +0000

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *  - writes the character n to stdout
+ * @b: Size character to print
+ *
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+printf ("%s\n", __FILE__);
+return (0);
+}

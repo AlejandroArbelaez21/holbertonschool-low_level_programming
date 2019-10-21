1e5f429c02a62ce23712441a50e06228f018ffa3
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-10-21 21:52:27 +0000

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_dog - writes the character n to stdout
+ * @d: Size character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+	{
+	return;
+	}
+else
+
+}

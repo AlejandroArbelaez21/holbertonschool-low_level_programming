5fd9e42104ea189c2530f76eee372c50fcde644b
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-10-21 20:06:34 +0000

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This the project 0x0E-structures_typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Short description
+ * @name: name if member
+ * @age: age of member
+ * @owner: owner of member
+ *
+ * Description: description of my dog Django
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};

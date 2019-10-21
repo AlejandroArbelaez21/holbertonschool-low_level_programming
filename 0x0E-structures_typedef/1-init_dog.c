308bd4998b433ec68eb42f7d4c2cb2a705dea235
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-10-21 21:00:37 +0000

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - writes the character n to stdout
+ * @d: Size character to print
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}

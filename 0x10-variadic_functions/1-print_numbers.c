7479b8452180de035a6416cfb7e96cc35177c6dc
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-10-25 22:09:51 +0000

    0x10. C - Variadic functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - writes the character n to stdout
+ * @n: element of the sum
+ * @separator: is a ", "
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list ap;
+unsigned int i;
+if (separator == NULL)
+	{
+	}
+va_start(ap, n);
+for (i = 0; i < n; i++)
+	{
+	if (i < n)
+		{
+		printf("%d", va_arg(ap, int));
+		}
+	if (i < (n - 1))
+		{
+		printf("%s", separator);
+		}
+	}
+va_end(ap);
+printf("\n");
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - writes the character n to stdout
+ * @n: element of the strings
+ * @separator: is a ", "
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list ap;
+unsigned int i;
+if (separator == NULL)
+	{
+	}
+va_start(ap, n);
+for (i = 0; i < n; i++)
+	{
+char *x = va_arg(ap, char*);
+	if (x != NULL)
+		{
+		printf("%s", x);
+		}
+	else
+		{
+		printf("(nil)");
+		}
+	if (i < (n - 1))
+		{
+		printf("%s", separator);
+		}
+	}
+va_end(ap);
+printf("\n");
+}

3ac9131c0f268cb475685e0a238ea4b47a15f5fe
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2019-10-28 03:54:28 +0000

    0x10. C - Variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -43,6 +43,9 @@ while (format[i])
 		default:
 		break;
 		}
+	if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' ||
+	 format[i] == 'f' || format[i] == 's'))
+	printf(", ");
 	i++;
 	}
 printf("\n");

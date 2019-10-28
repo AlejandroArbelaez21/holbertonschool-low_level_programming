eebc56c84a104c713247d76cdd4ee90a749f6597
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2019-10-28 04:15:21 +0000

    0x10. C - Variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,9 +13,10 @@ void print_all(const char * const format, ...)
 va_list ap;
 int i = 0;
 char *s;
-
 va_start(ap, format);
-
+while (format == NULL)
+	{
+	}
 while (format[i])
 	{
 	switch (format[i])
@@ -23,15 +24,12 @@ while (format[i])
 		case 'c':
 		printf("%c", va_arg(ap, int));
 		break;
-
 		case  'i':
 		printf("%d", va_arg(ap, int));
 		break;
-
 		case 'f':
-		printf("%f", (float)va_arg(ap, double));
+		printf("%f", va_arg(ap, double));
 		break;
-
 		case 's':
 		s = va_arg(ap, char *);
 		if (s == NULL)
@@ -40,7 +38,6 @@ while (format[i])
 			}
 		printf("%s", s);
 		break;
-
 		default:
 		break;
 		}

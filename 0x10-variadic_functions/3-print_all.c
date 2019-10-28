b6312c661464d38b8a23abff2a1402eceeba9df3
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2019-10-28 03:04:27 +0000

    0x10. C - Variadic functions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_all - writes the character n to stdout
+ * @format: element of the strings
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_all(const char * const format, ...)
+{
+va_list ap;
+int i = 0;
+char *s;
+va_start(ap, format);
+
+while (format[i])
+	{
+	switch (format[i])
+		{
+		case 'c':
+		printf("%c", va_arg(ap, int));
+		break;
+
+		case  'i':
+		printf("%i", va_arg(ap, int));
+		break;
+
+		case 'f':
+		printf("%f", va_arg(ap, double));
+		break;
+
+		case 's':
+		s = va_arg(ap, char *);
+		if (s == NULL)
+			{
+			printf("(nil)");
+			}
+		printf("%s", s);
+		break;
+
+		default:
+		break;
+		}
+	i++;
+	}
+printf("\n");
+va_end(ap);
+}

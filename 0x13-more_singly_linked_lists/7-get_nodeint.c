be2f52d25813c7eadd4f07738927371096bb31b9
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-11-05 20:56:23 +0000

    add the point 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,11 +4,27 @@
 /**
  * get_nodeint_at_index - writes the character n to stdout
  * @head: head the node
- *
+ * @index: is the index of the node
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+listint_t *tmp;
+unsigned int i = 0;
 
+if (head == NULL)
+	{
+	tmp = head;
+	while (tmp != NULL)
+	{
+		if (i == index)
+		{
+		return (tmp);
+		}
+	i++;
+	tmp = tmp->next;
+	}
+	}
+return (NULL);
 }

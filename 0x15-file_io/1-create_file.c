532768c55f11abc05c51e9ce219e5d70c93efbb8
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-11-12 22:49:42 +0000

    add the correction the point number 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,13 +17,10 @@ int create_file(const char *filename, char *text_content)
 {
 int fd, i;
 
-for (i = 0; text_content[i] != '\0'; i++)
-	{
-	}
 if (filename == NULL)
-        {
-        return (-1);
-        }
+	{
+	return (-1);
+	}
 fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd == -1)
 		{
@@ -35,6 +32,9 @@ if (text_content == NULL)
 	}
 else
 	{
+	for (i = 0; text_content[i] != '\0'; i++)
+	{
+	}
 	write(fd, text_content, i);
 	}
 close(fd);

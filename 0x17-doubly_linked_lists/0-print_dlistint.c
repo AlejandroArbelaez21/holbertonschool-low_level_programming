f60768b83baaef86237bcb67a76a7ac67a87f65e
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2019-12-15 01:51:10 +0000

    fixing up the task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,18 +10,16 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 int x = 0;
+
+if (h == NULL)
+	{
+	return (x);
+	}
 while (h)
 	{
-	if (h->n == '\0')
-		{
-		return (x);
-		}
-	else
-		{
-		printf("%d\n", h->n);
-		}
-x++;
-h = h->next;
+	printf("%d\n", h->n);
+	x++;
+	h = h->next;
 	}
 return (x);
 }

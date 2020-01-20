19c68eaafb040381e16f241ebecb9f05c17a5272
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2020-01-20 03:23:27 +0000

    correction

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,28 +16,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 hash_node_t *new_hash;
 unsigned long int idx;
 
-if (ht || key || value)
+if (!ht || !key || !value)
 {
-	idx = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[idx] && strcmp(ht->array[idx]->key, key) == 0)
-	{
-		ht->array[idx]->value = strdup(value);
-		return (1);
-	}
-
-	new_hash = malloc(sizeof(hash_node_t));
-	if (new_hash == NULL)
-	{
-		return (0);
-	}
-	new_hash->key = strdup(key);
-	new_hash->value = strdup(value);
-	new_hash->next = ht->array[idx];
-	ht->array[idx] = new_hash;
+	return (0);
+}
+idx = key_index((const unsigned char *)key, ht->size);
+if (ht->array[idx] && strcmp(ht->array[idx]->key, key) == 0)
+{
+	ht->array[idx]->value = strdup(value);
 	return (1);
 }
-else
-	{
+new_hash = malloc(sizeof(hash_node_t));
+if (new_hash == NULL)
+{
 	return (0);
-	}
+}
+new_hash->key = strdup(key);
+new_hash->value = strdup(value);
+new_hash->next = ht->array[idx];
+ht->array[idx] = new_hash;
+return (1);
 }

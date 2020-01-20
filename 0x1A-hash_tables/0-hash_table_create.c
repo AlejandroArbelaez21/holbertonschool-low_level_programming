0ddb4b06cde2d08a9dbe349bc9ca5718fc8c3626
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2020-01-20 02:06:03 +0000

    correction task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,6 +13,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *new_hash;
 
+if (size == 0)
+	{
+	return (NULL);
+	}
 new_hash = malloc(sizeof(hash_table_t));
 if (new_hash == NULL)
 	{
